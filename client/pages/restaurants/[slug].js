67a87fe2c1a3ace170152e5418806d1c15defb5b
Warre <warre.vandeveire@gmail.com>
2022-10-19 10:44:46 +0200

    fix restaurant page generation

--- client/pages/restaurants/[slug].js
+++ client/pages/restaurants/[slug].js
@@ -1,12 +1,12 @@
-import delve from "dlv";
-import Layout from "../../components/layout";
-import RestaurantContent from "../../components/pages/restaurant/RestaurantContent";
-import BlockManager from "../../components/shared/BlockManager";
-import { getStrapiURL, handleRedirection } from "../../utils";
-import { getLocalizedParams } from "../../utils/localize";
+import delve from 'dlv';
+import Layout from '../../components/layout';
+import RestaurantContent from '../../components/pages/restaurant/RestaurantContent';
+import BlockManager from '../../components/shared/BlockManager';
+import { getStrapiURL, handleRedirection } from '../../utils';
+import { getLocalizedParams } from '../../utils/localize';
 
 const Restaurant = ({ global, pageData, preview }) => {
-  const blocks = delve(pageData, "attributes.blocks");
+  const blocks = delve(pageData, 'attributes.blocks');
   return (
     <>
       <Layout
@@ -22,11 +22,27 @@ const Restaurant = ({ global, pageData, preview }) => {
   );
 };
 
-export async function getServerSideProps(context) {
-  const { locale } = getLocalizedParams(context.query);
+export async function getStaticPaths() {
+  const apiURL = getStrapiURL('/restaurants/');
+  const res = await fetch(apiURL);
+  const restaurants = await res.json();
+
+  const paths = restaurants.data.map((restaurant) => ({
+    params: {
+      id: restaurant.id,
+      lang: 'en',
+      slug: restaurant.attributes.slug,
+    },
+  }));
+
+  return { paths, fallback: false };
+}
+
+export async function getStaticProps(context) {
+  const { locale } = getLocalizedParams(context.params);
   const preview = context.preview
-    ? "&publicationState=preview&published_at_null=true"
-    : "";
+    ? '&publicationState=preview&published_at_null=true'
+    : '';
   const res = await fetch(
     getStrapiURL(
       `/restaurants?filters[slug]=${context.params.slug}&locale=${locale}${preview}&populate[reviews][populate]=author,author.picture&populate[information][populate]=opening_hours,location&populate[images][fields]=url&populate[category][fields]=name&populate[localizations]=*&populate[socialNetworks]=*&populate[blocks][populate]=restaurants.images,header,faq,buttons.link`
@@ -38,7 +54,7 @@ export async function getServerSideProps(context) {
     return handleRedirection(
       context.params.slug,
       context.preview,
-      "restaurants"
+      'restaurants'
     );
   }
 
